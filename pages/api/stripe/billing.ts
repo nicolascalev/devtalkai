aaf802eb394fff656aaa9d87c4624e9c9f82fd0e
nicolascalev <nicolascalevg@gmail.com>
2023-05-23 09:00:19 -0400

    🐛 get fresh user to redirect to stripe portal

--- pages/api/stripe/billing.ts
+++ pages/api/stripe/billing.ts
@@ -1,20 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import stripe from "../../../utils/stripe.client";
-import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
+import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 import { UserWithNestedProperties } from "../../../types/types";
 
-export default withApiAuthRequired(async function billingHanlder(req: NextApiRequest, res: NextApiResponse) {
+export default withApiAuthRequired(async function billingHanlder(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const session: any = await getSession(req, res);
   const user: UserWithNestedProperties = session.user;
 
+  const freshUser = await prisma.user.findUnique({
+    where: { id: user.id },
+  });
+
+  if (!freshUser || !freshUser.stripeCustomerId) {
+    return res.status(400).send("User does not have a stripe customer id");
+  }
+
   try {
     const portal = await stripe.billingPortal.sessions.create({
-      customer: user.stripeCustomerId as string,
+      customer: freshUser!.stripeCustomerId as string,
       return_url: process.env.NEXT_PUBLIC_BASE_URL + "/",
-    })
+    });
     return res.redirect(portal.url);
   } catch (err) {
     return res.status(500).json(err);
   }
 });
-
